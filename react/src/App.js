d214e72cc7e9278c8cadcf8d152351003318038c
motatoes <moe.habib9@gmail.com>
2022-08-04 14:32:03 +0100

    add react

---
+++ react/src/App.js
@@ -0,0 +1,22 @@
+import logo from './digger-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-logo-container">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <h3>
+          Congratulations! You have deployed a service on Digger
+        </h3>
+        <h4 className="App-link">
+          This is the default output of our sample react app. Lets go dig!
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
